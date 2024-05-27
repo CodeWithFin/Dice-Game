455b0adb7720b046f726f218c8d6b2f836527f49
Finley <finley.mwachia12@gmail.com>
2024-05-27 11:57:10 +0300

    added new var randomnumber 2 and image source1 ,imagesource 2

--- Dicee+Challenge+-+Starting+Files/index.js
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -1,2 +1,8 @@
-var randomNumber1 = Math.floor(Math.random()*6) +1;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 // console.log(randomNumber1);
+
+var imageSource1 = "./images/"+"dice"+randomNumber1+".png"
+// console.log(diceImage);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var imageSource2 = "./images/"+"dice"+randomNumber2+".png"
